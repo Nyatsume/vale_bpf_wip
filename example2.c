9a614be31e8b8bacfa9855138eb30094ef947e02
Naoyuki Tachibana <nyatsume@shinano2.ht.sfc.keio.ac.jp>
2018-07-24 21:53:29 +0900

    for demonstoration

---
+++ example2.c
@@ -0,0 +1,214 @@
+#include <net/vale_bpf_native_api.h>
+#define ETH_P_IP 0x0800
+#define IPPROTO_TCP 6
+#define IPPROTO_UDP 17
+
+#define SERVER_IP1 0x030000a0 // 10.0.0.3 network byteorder
+#define SERVER_IP2 0x020000a0 // 10.0.0.2 network byteorder
+
+struct eth {
+  uint8_t dst[6];
+  uint8_t src[6];
+  uint16_t type;
+};
+
+struct ip {
+  uint8_t verihl;
+  uint8_t tos;
+  uint16_t tot_length;
+  uint16_t id;
+  uint16_t flagofs;
+  uint8_t ttl;
+  uint8_t proto;
+  uint16_t csum;
+  uint32_t saddr;
+  uint32_t daddr;
+};
+
+struct tcp {
+  uint16_t sport;
+  uint16_t dport;
+  uint32_t seq;
+  uint32_t ack_seq;
+  uint16_t res1:4;
+  uint16_t doff:4;
+  uint16_t fin:1;
+  uint16_t syn:1;
+  uint16_t rst:1;
+  uint16_t psh:1;
+  uint16_t ack:1;
+  uint16_t urg:1;
+  uint16_t res2:2;
+  uint16_t window;
+  uint16_t csum;
+  uint16_t urg_ptr;
+};
+
+struct udp {
+  uint16_t sport;
+  uint16_t dport;
+  uint16_t length;
+  uint16_t csum;
+};
+
+#define FNV_32_PRIME ((uint32_t) 0x01000193UL)
+static inline __attribute__((always_inline)) uint32_t
+fnv_32_hash(const void *buf, size_t len, uint32_t hval)
+{
+  const u_int8_t *s = (const u_int8_t *)buf;
+
+  while (len-- != 0) {
+    hval *= FNV_32_PRIME;
+    hval ^= *s++;
+  }
+
+  return hval;
+}
+
+static inline __attribute__((always_inline)) uint16_t
+csum16_add(uint16_t csum, uint16_t addend)
+{
+	  uint16_t ret = csum + addend;
+	    return ret + (ret < addend);
+}
+
+static inline __attribute__((always_inline)) uint16_t
+csum16_sub(uint16_t csum, uint16_t addend)
+{
+	  return csum16_add(csum, ~addend);
+}
+
+static inline __attribute__((always_inline)) void
+rewrite_addr_ipv4(struct ip *ip, uint32_t rewrite_addr)
+{
+	// IPヘッダの中のアドレス書き換え & チェックサム再計算
+  ip->csum = csum16_sub(ip->csum, ~(ip->saddr & 0xffff));
+  ip->csum = csum16_sub(ip->csum, ~(ip->saddr >> 16));
+  ip->csum = csum16_add(ip->csum, ~(rewrite_addr & 0xffff));
+  ip->csum = csum16_add(ip->csum, ~(rewrite_addr >> 16));
+  
+}
+
+static inline __attribute__((always_inline)) void
+rewrite_addr_udp(struct udp *udp, struct ip *ip, uint32_t rewrite_addr)
+{
+	rewrite_addr_ipv4(ip, rewrite_addr);
+	// UDPのチェックサム再計算
+  udp->csum = csum16_sub(udp->csum, ~(ip->saddr & 0xffff));
+  udp->csum = csum16_sub(udp->csum, ~(ip->saddr >> 16));
+  udp->csum = csum16_add(udp->csum, ~(rewrite_addr & 0xffff));
+  udp->csum = csum16_add(udp->csum, ~(rewrite_addr >> 16));
+	
+}
+
+static inline __attribute__((always_inline)) void
+rewrite_addr_tcp(struct tcp *tcp, struct ip *ip, uint32_t rewrite_addr)
+{
+	rewrite_addr_ipv4(ip, rewrite_addr);
+	// TCPのチェックサム再計算
+  tcp->csum = csum16_sub(tcp->csum, ~(ip->saddr & 0xffff));
+  tcp->csum = csum16_sub(tcp->csum, ~(ip->saddr >> 16));
+  tcp->csum = csum16_add(tcp->csum, ~(rewrite_addr & 0xffff));
+  tcp->csum = csum16_add(tcp->csum, ~(rewrite_addr >> 16));
+}
+
+uint32_t
+lookup(struct vale_bpf_native_md *ctx)
+{
+  void *data_end = (void *)(long)ctx->data_end;
+  void *data = (void *)(long)ctx->data;
+  uint32_t ingress_port = ctx->ingress_port;
+
+  if (data + sizeof(struct eth) > data_end) {
+    return VALE_BPF_DROP;
+  }
+
+  struct eth *eth = data;
+  if (eth->type != ETH_P_IP) {
+    return VALE_BPF_DROP;
+  }
+
+  struct ip *ip = (struct ip *)(eth + 1);
+  if (ip + 1 > data_end) {
+    return VALE_BPF_DROP;
+  }
+
+  // ごにょごにょ IPアドレスとUDPのポートをそれぞれ出力
+  struct udp *udp;
+  struct tcp *tcp;
+  uint16_t port;
+
+  if (ip->proto == IPPROTO_UDP) {
+    udp = (struct udp *)(ip + 1)
+    if (udp + 1 > data_end) {
+      return VALE_BPF_DROP;
+    }
+    port = udp->sport;
+  } else if (ip->proto == IPPROTO_TCP)
+    tcp = (struct tcp *)(ip + 1)
+    if (tcp + 1 > data_end) {
+      return VALE_BPF_DROP;
+    }
+    port = tcp->sport;
+  } else {
+    return VALE_BPF_DROP;
+  }
+
+  // サーバー側から来たパケット
+  if (ingress_port != 0) {
+    if (ip->proto == IPPROTO_UDP) {
+      rewrite_addr_udp(udp, ip, rewrite_addr);
+    } else if (ip->proto == IPPROTO_TCP) {
+      rewrite_addr_tcp(tcp, ip, rewrite_addr);
+    }
+
+    return 0;
+  }
+   
+  // これいこうの処理はクライアント側からきたやつ
+
+  // IPアドレスとUDPのポート番号を元にハッシュ値を計算
+  uint32_t hash = 0;
+  hash = fnv_32_hash(ip->saddr, sizeof(uint32_t), hash);
+  hash = fnv_32_hash(port, sizeof(uint32_t), hash);
+  hash = fnv_32_hash(ip->proto, sizeof(uint8_t), hash);
+
+  // % 2 で丸める
+  uint32_t mod = hash % 2;
+  if (mod == 0) {
+    if (ip->proto == IPPROTO_UDP) {
+      rewrite_addr_udp(udp, ip, rewrite_addr);
+    } else if (ip->proto == IPPROTO_TCP) {
+      rewrite_addr_tcp(tcp, ip, rewrite_addr);
+    }
+      
+		  
+
+
+
+	  return // なんとか
+  } else if (mod == 1) {
+      if (ip->proto == IPPROTO_UDP) {
+        rewrite_addr_udp(udp, ip, rewrite_addr);
+      } else if (ip->proto == IPPROTO_TCP) {
+        rewrite_addr_tcp(tcp, ip, rewrite_addr);
+      }
+	  // IP書き換え、チェックサム計算
+	  return // なんとか
+  }
+
+  /*
+  bpf_trace_printk("%d,%d\n",bpf_ntohs(udp->sport),bpf_ntohs(udp->dport));
+  ip->csum = csum16_sub(ip->csum, ~(ip->saddr & 0xffff));
+  ip->csum = csum16_sub(ip->csum, ~(ip->saddr >> 16));
+  udp->csum = csum16_sub(udp->csum, ~(ip->saddr & 0xffff));
+  udp->csum = csum16_sub(udp->csum, ~(ip->saddr >> 16));
+  ip->saddr=123456;  
+  ip->csum = csum16_add(ip->csum, ~(ip->saddr & 0xffff));
+  ip->csum = csum16_add(ip->csum, ~(ip->saddr >>16));
+  udp->csum = csum16_add(udp->csum, ~(ip->saddr & 0xffff));
+  udp->csum = csum16_add(udp->csum, ~(ip->saddr >> 16));
+  */
+
+  return VALE_BPF_DROP;
+}
