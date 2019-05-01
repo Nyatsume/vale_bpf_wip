eb0fa3518cf2bac25b429cb3483e07bf13fd2a31
nyatsume <xynaoyuki@gmail.com>
2019-05-01 19:31:47 +0900

    fixed indents

--- l4lb.c
+++ l4lb.c
@@ -1,13 +1,11 @@
-iinclude <net/vale_bpf_native_api.h>
+iinclude<net / vale_bpf_native_api.h>
 #define ETH_P_IP 0x0008
 #define IPPROTO_TCP 6
 #define IPPROTO_UDP 17
 #define DIRECTION_IN 0
 #define DIRECTION_OUT 1
 
-
-
-struct eth {
+    struct eth {
   uint8_t dst[6];
   uint8_t src[6];
   uint16_t type;
@@ -31,15 +29,15 @@ struct tcp {
   uint16_t dport;
   uint32_t seq;
   uint32_t ack_seq;
-  uint16_t res1:4;
-  uint16_t doff:4;
-  uint16_t fin:1;
-  uint16_t syn:1;
-  uint16_t rst:1;
-  uint16_t psh:1;
-  uint16_t ack:1;
-  uint16_t urg:1;
-  uint16_t res2:2;
+  uint16_t res1 : 4;
+  uint16_t doff : 4;
+  uint16_t fin : 1;
+  uint16_t syn : 1;
+  uint16_t rst : 1;
+  uint16_t psh : 1;
+  uint16_t ack : 1;
+  uint16_t urg : 1;
+  uint16_t res2 : 2;
   uint16_t window;
   uint16_t csum;
   uint16_t urg_ptr;
@@ -59,12 +57,11 @@ struct val {
   uint8_t _pad[5];
 };
 
-BPF_TABLE("array",uint32_t, struct val, table, 4);
+BPF_TABLE("array", uint32_t, struct val, table, 4);
 
-#define FNV_32_PRIME ((uint32_t) 0x01000193UL)
-static inline __attribute__((always_inline)) uint32_t
-fnv_32_hash(const void *buf, size_t len, uint32_t hval)
-{
+#define FNV_32_PRIME ((uint32_t)0x01000193UL)
+static inline __attribute__((always_inline)) uint32_t fnv_32_hash(
+    const void *buf, size_t len, uint32_t hval) {
   const u_int8_t *s = (const u_int8_t *)buf;
 
   while (len-- != 0) {
@@ -75,61 +72,51 @@ fnv_32_hash(const void *buf, size_t len, uint32_t hval)
   return hval;
 }
 
-static inline __attribute__((always_inline)) uint16_t
-csum16_add(uint16_t csum, uint16_t addend)
-{
-	  uint16_t ret = csum + addend;
-	    return ret + (ret < addend);
+static inline __attribute__((always_inline)) uint16_t csum16_add(
+    uint16_t csum, uint16_t addend) {
+  uint16_t ret = csum + addend;
+  return ret + (ret < addend);
 }
 
-static inline __attribute__((always_inline)) uint16_t
-csum16_sub(uint16_t csum, uint16_t addend)
-{
-	  return csum16_add(csum, ~addend);
+static inline __attribute__((always_inline)) uint16_t csum16_sub(
+    uint16_t csum, uint16_t addend) {
+  return csum16_add(csum, ~addend);
 }
 
-static inline __attribute__((always_inline)) void
-rewrite_addr_ipv4(struct ip *ip, uint32_t rewrite_addr,uint32_t *addr)
-{
-	// IPヘッダの中のアドレス書き換え & チェックサム再計算
+static inline __attribute__((always_inline)) void rewrite_addr_ipv4(
+    struct ip *ip, uint32_t rewrite_addr, uint32_t *addr) {
+  // IPヘッダの中のアドレス書き換え & チェックサム再計算
 
-  
   ip->csum = csum16_sub(ip->csum, ~(*addr & 0xffff));
   ip->csum = csum16_sub(ip->csum, ~(*addr >> 16));
   ip->csum = csum16_add(ip->csum, ~(rewrite_addr & 0xffff));
   ip->csum = csum16_add(ip->csum, ~(rewrite_addr >> 16));
-   *addr = rewrite_addr;
- }
-
-
-static inline __attribute__((always_inline)) void
-rewrite_addr_udp(struct udp *udp, struct ip *ip, uint32_t rewrite_addr, uint32_t *addr)
-{
-	
-	// UDPのチェックサム再計算
-  udp->csum = csum16_sub(udp->csum, ~(*addr & 0xffff));
-  udp->csum = csum16_sub(udp->csum, ~(*addr >> 16));
-  
-	rewrite_addr_ipv4(ip, rewrite_addr, addr);
-  udp->csum = csum16_add(udp->csum, ~(rewrite_addr & 0xffff));
-  udp->csum = csum16_add(udp->csum, ~(rewrite_addr >> 16));
-	
+  *addr = rewrite_addr;
 }
 
-static inline __attribute__((always_inline)) void
-rewrite_addr_tcp(struct tcp *tcp, struct ip *ip, uint32_t rewrite_addr, uint32_t *addr)
-{
+static inline __attribute__((always_inline)) void rewrite_addr_udp(
+    struct udp *udp, struct ip *ip, uint32_t rewrite_addr, uint32_t *addr) {
+  // UDPのチェックサム再計算
+  udp->csum = csum16_sub(udp->csum, ~(*addr & 0xffff));
+  udp->csum = csum16_sub(udp->csum, ~(*addr >> 16));
 
-	// TCPのチェックサム再計算
+  rewrite_addr_ipv4(ip, rewrite_addr, addr);
+  udp->csum = csum16_add(udp->csum, ~(rewrite_addr & 0xffff));
+  udp->csum = csum16_add(udp->csum, ~(rewrite_addr >> 16));
+}
+
+static inline __attribute__((always_inline)) void rewrite_addr_tcp(
+    struct tcp *tcp, struct ip *ip, uint32_t rewrite_addr, uint32_t *addr) {
+  // TCPのチェックサム再計算
   tcp->csum = csum16_sub(tcp->csum, ~(*addr & 0xffff));
   tcp->csum = csum16_sub(tcp->csum, ~(*addr >> 16));
-	rewrite_addr_ipv4(ip, rewrite_addr, addr);
+  rewrite_addr_ipv4(ip, rewrite_addr, addr);
   tcp->csum = csum16_add(tcp->csum, ~(rewrite_addr & 0xffff));
   tcp->csum = csum16_add(tcp->csum, ~(rewrite_addr >> 16));
 }
 
-static inline __attribute__((always_inline)) uint32_t calc_hash(uint32_t sip, uint32_t dip, uint16_t sport, uint16_t dport, uint8_t proto)
-{
+static inline __attribute__((always_inline)) uint32_t calc_hash(
+    uint32_t sip, uint32_t dip, uint16_t sport, uint16_t dport, uint8_t proto) {
   uint32_t hash = 0;
   hash = fnv_32_hash(&sip, sizeof(uint32_t), hash);
   hash = fnv_32_hash(&dip, sizeof(uint32_t), hash);
@@ -139,16 +126,13 @@ static inline __attribute__((always_inline)) uint32_t calc_hash(uint32_t sip, ui
   return hash % 3;
 }
 
-uint32_t
-lookup(struct vale_bpf_native_md *ctx)
-{
-
+uint32_t lookup(struct vale_bpf_native_md *ctx) {
   void *data_end = (void *)(long)ctx->data_end;
   void *data = (void *)(long)ctx->data;
   uint32_t ingress_port = ctx->ingress_port;
 
   if (data + sizeof(struct eth) > data_end) {
-    bpf_trace_printk("pkt dropped\n");	 
+    bpf_trace_printk("pkt dropped\n");
     return VALE_BPF_DROP;
   }
 
@@ -160,10 +144,9 @@ lookup(struct vale_bpf_native_md *ctx)
 
   struct ip *ip = (struct ip *)(eth + 1);
   if (ip + 1 > data_end) {
-    bpf_trace_printk("pkt dropped\n");	 
+    bpf_trace_printk("pkt dropped\n");
     return VALE_BPF_DROP;
   }
-
 
   /*
   struct val *v;
@@ -179,7 +162,7 @@ lookup(struct vale_bpf_native_md *ctx)
   struct udp *udp;
   struct tcp *tcp;
   uint16_t port;
-  
+
   struct val *v;
   uint32_t key = 3;
   uint32_t mod;
@@ -187,7 +170,7 @@ lookup(struct vale_bpf_native_md *ctx)
   if (ip->proto == IPPROTO_UDP) {
     udp = (struct udp *)(ip + 1);
     if (udp + 1 > data_end) {
-	    bpf_trace_printk("pkt dropped\n");
+      bpf_trace_printk("pkt dropped\n");
       return VALE_BPF_DROP;
     }
     port = udp->sport;
@@ -195,14 +178,14 @@ lookup(struct vale_bpf_native_md *ctx)
     if (ingress_port != 0) {
       v = table.lookup(&key);
       if (v == NULL) {
-	return VALE_BPF_DROP;
+        return VALE_BPF_DROP;
       }
       rewrite_addr_udp(udp, ip, v->rip, &ip->saddr);
       return 0;
     }
     mod = calc_hash(ip->saddr, ip->daddr, udp->sport, udp->dport, ip->proto);
     v = table.lookup(&mod);
-    if(v == NULL) {
+    if (v == NULL) {
       return VALE_BPF_DROP;
     }
     rewrite_addr_udp(udp, ip, v->rip, &ip->daddr);
@@ -215,32 +198,31 @@ lookup(struct vale_bpf_native_md *ctx)
     }
     port = tcp->sport;
     if (ingress_port != 0) {
-
       v = table.lookup(&key);
       if (v == NULL) {
-				return VALE_BPF_DROP;
+        return VALE_BPF_DROP;
       }
       rewrite_addr_tcp(tcp, ip, v->rip, &ip->saddr);
-			//vip 10.0.0.1 a0:36:9f:1a:2f:24
-			eth->src[0] = 0xa0;
-			eth->src[1] = 0x36;
-			eth->src[2] = 0x9f;
-			eth->src[3] = 0x1a;
-			eth->src[4] = 0x2f;
-			eth->src[5] = 0x24;
-			//client vip 10.0.0.10 a0:36:9f:1a:2d:30
-			eth->dst[0] = 0xa0;
-			eth->dst[1] = 0x36;
-			eth->dst[2] = 0x9f;
-			eth->dst[3] = 0x1a;
-			eth->dst[4] = 0x2d;
-			eth->dst[5] = 0x30;
+      // vip 10.0.0.1 a0:36:9f:1a:2f:24
+      eth->src[0] = 0xa0;
+      eth->src[1] = 0x36;
+      eth->src[2] = 0x9f;
+      eth->src[3] = 0x1a;
+      eth->src[4] = 0x2f;
+      eth->src[5] = 0x24;
+      // client vip 10.0.0.10 a0:36:9f:1a:2d:30
+      eth->dst[0] = 0xa0;
+      eth->dst[1] = 0x36;
+      eth->dst[2] = 0x9f;
+      eth->dst[3] = 0x1a;
+      eth->dst[4] = 0x2d;
+      eth->dst[5] = 0x30;
       return 0;
     }
 
     mod = calc_hash(ip->saddr, ip->daddr, tcp->sport, tcp->dport, ip->proto);
     v = table.lookup(&mod);
-    if(v == NULL) {
+    if (v == NULL) {
       return VALE_BPF_DROP;
     }
     rewrite_addr_tcp(tcp, ip, v->rip, &ip->daddr);
@@ -258,13 +240,12 @@ lookup(struct vale_bpf_native_md *ctx)
     eth->src[4] = 0x2f;
     eth->src[5] = 0x26;
     return v->port;
-    
+
   } else {
-　  bpf_trace_printk("pkt dropped\n");
+    　 bpf_trace_printk("pkt dropped\n");
     return VALE_BPF_DROP;
   }
 
-   
   // これいこうの処理はクライアント側からきたやつ
 
   // IPアドレスとUDPのポート番号を元にハッシュ値を計算
@@ -283,10 +264,10 @@ lookup(struct vale_bpf_native_md *ctx)
     } else if (ip->proto == IPPROTO_TCP) {
       rewrite_addr_tcp(tcp, ip, SERVER_IP2, &ip->daddr);
     }
-    
 
 
-	  return 1; // なんとか
+
+          return 1; // なんとか
   } else if (mod == 1) {
      eth->dst[0] = 0xa0;
      eth->dst[1] = 0x36;
@@ -300,9 +281,9 @@ lookup(struct vale_bpf_native_md *ctx)
       } else if (ip->proto == IPPROTO_TCP) {
         rewrite_addr_tcp(tcp, ip, SERVER_IP3, &ip->daddr);
       }
-	  // IP書き換え、チェックサム計算
-	 
-	  return 2; // なんとか
+          // IP書き換え、チェックサム計算
+
+          return 2; // なんとか
   }
 
   /*
@@ -311,7 +292,7 @@ lookup(struct vale_bpf_native_md *ctx)
   ip->csum = csum16_sub(ip->csum, ~(ip->saddr >> 16));
   udp->csum = csum16_sub(udp->csum, ~(ip->saddr & 0xffff));
   udp->csum = csum16_sub(udp->csum, ~(ip->saddr >> 16));
-  ip->saddr=123456;  
+  ip->saddr=123456;
   ip->csum = csum16_add(ip->csum, ~(ip->saddr & 0xffff));
   ip->csum = csum16_add(ip->csum, ~(ip->saddr >>16));
   udp->csum = csum16_add(udp->csum, ~(ip->saddr & 0xffff));
